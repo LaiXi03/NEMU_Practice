0cf09489a927698698589de05dbb98d4183840df
Laixi Liu <liulaixi.byte@foxmail.com>
2024-09-07 14:40:11 +0800

    implement difftest

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -15,10 +15,21 @@
 
 #include <isa.h>
 #include <cpu/difftest.h>
+#include <stdbool.h>
 #include "../local-include/reg.h"
+#include "macro.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  int reg_num = ARRLEN(cpu.gpr);
+  for (int i = 0; i < reg_num; i++) {
+    if (ref_r->gpr[i] != cpu.gpr[i]) {
+      return false;
+    }
+  }
+  if (ref_r->pc != cpu.pc) {
+    return false;
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
